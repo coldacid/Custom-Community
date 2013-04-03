4bff196d6c758f28293506b54ce48bdaddaa4cee
konradsroka <contact@konradsroka.com>
2013-04-04 04:12:36 +0800

    added new image for get the pro tab

--- admin/get-pro.php
+++ admin/get-pro.php
@@ -7,7 +7,7 @@
 		 <div id="cap_getpro">
 			<div class="getpro_content">
 			    <a href="http://themekraft.com/shop/custom-community-pro/" title="Custom Community Pro" target="_blank">
-			    <img src="<?php echo get_template_directory_uri(); ?>/_inc/images/get-pro.jpg" width="861" height="600" style="margin:10px 0;" />
+			    <img src="<?php echo get_template_directory_uri(); ?>/_inc/images/get-pro.jpg" width="861" height="1200" style="margin:10px 0;" />
 			    </a> 
 			</div>
 		</div>
